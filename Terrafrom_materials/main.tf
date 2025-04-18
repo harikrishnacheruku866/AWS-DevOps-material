10827175cd1701fd88b235ebbc75f0ef01257e12
harikrishnacheruku866 <71976282+harikrishnacheruku866@users.noreply.github.com>
2025-04-18 14:23:18 +0530

    Create main.tf

---
+++ Terrafrom_materials/main.tf
@@ -0,0 +1,11 @@
+PROJECT-ec2-instance-creation/main.tf
+
+
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
